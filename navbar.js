fbd6cd9de609b09b38c4f0704830d1ed0268abde
Elias <nyelias21@gmail.com>
2023-09-29 00:22:37 -0400

    Initial Commit

---
+++ navbar.js
@@ -0,0 +1,14 @@
+//reuse navbar
+
+document.getElementById('topnav').innerHTML = `
+    <div class ="topnav">
+        <div class = "topnavContainer">
+            <img class = "logo" src = "logo.png">
+            <div class="navLinks">
+                <a href="index.html">HOME</a>
+                <a href="#">THINGS TO DO</a>
+                <a href="#">EAT + DRNK</a>
+                <a href="thecrew.html">THE CREW</a>
+            </div>
+        </div>
+    </div>`;
